141e36a5894ed269c0da114a3bdfaae2b78d40ec
qowjdals2 <qowjsals2@gmail.com>
2024-06-09 21:53:29 +0900

    Update background.js
    Signed-off-by: qowjdals2 <qowjsals2@gmail.com>

--- js/background.js
+++ js/background.js
@@ -1,7 +1,8 @@
-const images = ["1.jpg", "2.jpg", "3.jpg"]
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const images = ["1.jpg","2.jpg","3.jpg"];
+const chosenImage = images[Math.floor(Math.random() * images.length]
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src='img/${chosenImage}';
+console.log(bgImage);
 
-document.body.appendChild(bgImage);
+document.body.appendChild(bgImage)
