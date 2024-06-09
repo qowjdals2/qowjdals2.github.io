fce26415ca562ec7ebb5368a6690a60f046b95d3
qowjdals2 <qowjsals2@gmail.com>
2024-06-09 21:51:44 +0900

    Update greetings.js
    Signed-off-by: qowjdals2 <qowjsals2@gmail.com>

--- js/greetings.js
+++ js/greetings.js
@@ -6,7 +6,7 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-  event.preventDefault(); // 폼 제출의 기본 동작을 막습니다 (페이지 새로고침 방지).
+  event.preventDefault(); 
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
@@ -21,11 +21,11 @@ function paintGreetings(username) {
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  // 저장된 이름이 없으면 로그인 폼을 보여줍니다.
+  
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  // 저장된 이름이 있으면 환영 메시지를 보여줍니다.
+  
   loginForm.classList.add(HIDDEN_CLASSNAME);
   paintGreetings(savedUsername);
 }
